e5efec89b8dff537fa4f45d651bf5eafb97e09c4
Breena Theseira <breenatheseira@yahoo.com>
2015-03-08 11:11:28 +0800

    Added: One admin using apuorientation@mail.com

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+admin = Admin.create(email: "apuorientation@mail.com", password: "123123123")
