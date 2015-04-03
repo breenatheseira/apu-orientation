c08c926df3e2ff879b58a11f63882f1dcc3fbde0
Breena Theseira <breenatheseira@yahoo.com>
2015-04-03 11:17:55 +0800

    Updated: Logout path for after_sign_out_path_for(resource) in Application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,7 +14,7 @@ class ApplicationController < ActionController::Base
 		end
 	end
 
-	# def after_sign_out_path_for(resource_or_scope)
-	# 	your_path
-	# end
+	def after_sign_out_path_for(resource_or_scope)
+		root_path
+	end
 end
