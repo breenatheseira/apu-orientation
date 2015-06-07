ef17ee330006ddc0d4d9124a821d82fcbe248a8e
Breena Theseira <breenatheseira@yahoo.com>
2015-06-07 13:46:49 +0800

    Updated: db/seeds could upload the documents

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,13 +12,13 @@ Student.create(email: "TP000001@apu.edu.my", password: "123123123", username: "T
 Student.create(email: "TP000002@apu.edu.my", password: "123123123", username: "TP000002", intake_code: "UCD1F1503EE", name: "Sally")
 
 # Foundation Students
-Document.create(name: "Student Handbook v5", document_type: "Student Handbook", document_url: "http://res.cloudinary.com/apuorientation/image/upload/v1433491698/q8yxblhza3g5qsowq99q.pdf", intake_code: "UCFF1503")
-Document.create(name: "FF1503 Fee Schedule", document_type: "Fee Schedule", document_url: "http://res.cloudinary.com/apuorientation/image/upload/v1433491718/jeaacnv7khqav5ttaxqn.pdf", intake_code: "UCFF1503")
-Document.create(name: "FF1503 Orientation Schedule", document_type: "Orientation Schedule", document_url: "http://res.cloudinary.com/apuorientation/image/upload/v1433491734/aapeb8odkuqi6v2qbwqf.pdf", intake_code: "UCFF1503")
-Document.create(name: "FF1503 Important Details", document_type: "Important Details", document_url: "http://res.cloudinary.com/apuorientation/image/upload/v1433491718/jeaacnv7khqav5ttaxqn.pdf", intake_code: "UCFF1503")
+Document.create(name: "Student Handbook v5", document_type: "Student Handbook", document_url: open("http://res.cloudinary.com/apuorientation/image/upload/v1433491698/q8yxblhza3g5qsowq99q.pdf"), intake_code: "UCFF1503")
+Document.create(name: "FF1503 Fee Schedule", document_type: "Fee Schedule", document_url: open("http://res.cloudinary.com/apuorientation/image/upload/v1433491718/jeaacnv7khqav5ttaxqn.pdf"), intake_code: "UCFF1503")
+Document.create(name: "FF1503 Orientation Schedule", document_type: "Orientation Schedule", document_url: open("http://res.cloudinary.com/apuorientation/image/upload/v1433491734/aapeb8odkuqi6v2qbwqf.pdf"), intake_code: "UCFF1503")
+Document.create(name: "FF1503 Important Details", document_type: "Important Details", document_url: open("http://res.cloudinary.com/apuorientation/image/upload/v1433491718/jeaacnv7khqav5ttaxqn.pdf"), intake_code: "UCFF1503")
 
 # Diploma Students
-Document.create(name: "Student Handbook v5", document_type: "Student Handbook", document_url: "http://res.cloudinary.com/apuorientation/image/upload/v1433491698/q8yxblhza3g5qsowq99q.pdf", intake_code: "UCD1F1503")
-Document.create(name: "D1F1503 Fee Schedule", document_type: "Fee Schedule", document_url: "http://res.cloudinary.com/apuorientation/image/upload/v1433491718/jeaacnv7khqav5ttaxqn.pdf", intake_code: "UCD1F1503")
-Document.create(name: "D1F1503 Orientation Schedule", document_type: "Orientation Schedule", document_url: "http://res.cloudinary.com/apuorientation/image/upload/v1433491734/aapeb8odkuqi6v2qbwqf.pdf", intake_code: "UCD1F1503")
-Document.create(name: "D1F1503 Important Details", document_type: "Important Details", document_url: "http://res.cloudinary.com/apuorientation/image/upload/v1433491734/aapeb8odkuqi6v2qbwqf.pdf", intake_code: "UCD1F1503")
+Document.create(name: "Student Handbook v5", document_type: "Student Handbook", document_url: open("http://res.cloudinary.com/apuorientation/image/upload/v1433491698/q8yxblhza3g5qsowq99q.pdf"), intake_code: "UCD1F1503")
+Document.create(name: "D1F1503 Fee Schedule", document_type: "Fee Schedule", document_url: open("http://res.cloudinary.com/apuorientation/image/upload/v1433491718/jeaacnv7khqav5ttaxqn.pdf"), intake_code: "UCD1F1503")
+Document.create(name: "D1F1503 Orientation Schedule", document_type: "Orientation Schedule", document_url: open("http://res.cloudinary.com/apuorientation/image/upload/v1433491734/aapeb8odkuqi6v2qbwqf.pdf"), intake_code: "UCD1F1503")
+Document.create(name: "D1F1503 Important Details", document_type: "Important Details", document_url: open("http://res.cloudinary.com/apuorientation/image/upload/v1433491734/aapeb8odkuqi6v2qbwqf.pdf"), intake_code: "UCD1F1503")
