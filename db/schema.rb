dc82c3d52c3e2bb7dd1187070ec65ab067fc4dc0
Breena Theseira <breenatheseira@yahoo.com>
2015-06-12 15:23:23 +0800

    Updated: Removed the Recoverable module from student model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150605050016) do
+ActiveRecord::Schema.define(version: 20150612072155) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -41,12 +41,10 @@ ActiveRecord::Schema.define(version: 20150605050016) do
   end
 
   create_table "students", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
+    t.string   "email",                default: "", null: false
+    t.string   "encrypted_password",   default: "", null: false
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",        default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -63,6 +61,5 @@ ActiveRecord::Schema.define(version: 20150605050016) do
 
   add_index "students", ["authentication_token"], name: "index_students_on_authentication_token", unique: true
   add_index "students", ["email"], name: "index_students_on_email", unique: true
-  add_index "students", ["reset_password_token"], name: "index_students_on_reset_password_token", unique: true
 
 end
